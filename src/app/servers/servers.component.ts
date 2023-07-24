52e01465da794d83a3df50800448b5024ea52411
marpost <marijap@iborn.net>
2023-07-24 15:48:51 +0200

    enhancing 'ng-if' with an 'else' condition

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit{
   allowNewServer=false;
   serverCreationStatus='No server was created';
   serverName='TestServer';
+  serverCreated=false;
   constructor() {
     setTimeout(()=>{
       this.allowNewServer=true;
@@ -19,6 +20,7 @@ export class ServersComponent implements OnInit{
 
   }
   onCreateServer(){
+    this.serverCreated=true;
     this.serverCreationStatus='Server was created! Name is '+this.serverName;
   }
   onUpdateServerName(event:Event){
